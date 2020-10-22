89bb85c9fb054da6a0e9a93b7fa1bf8bc04a88dc
Jozsef Geng <jozsef.geng@fondof.de>
2020-10-22 12:32:23 +0200

    add SplittableCheckoutConstants

---
+++ src/FondOfSpryker/Shared/SplittableCheckout/SplittableCheckoutConstants.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace FondOfSpryker\Shared\SplittableCheckout;
+
+interface SplittableCheckoutConstants
+{
+    public const  QUOTE_SPLIT_QUOTE_ITEM_ATTRIBUTE = 'SPLITTABLE_CHECKOUT:QUOTE_SPLIT_QUOTE_ITEM_ATTRIBUTE';
+}
